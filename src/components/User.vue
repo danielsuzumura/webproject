c14aefda8b503d0ab8724f279d4b4e3335b1cd82
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-06-30 21:26:13 -0300

    Add user page side menu

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,53 +1,60 @@
 <template>
     <div id="container-user" v-if="user !== null">
         <div id="user-box">
-            <h1>User information</h1>
-            <table>
-                <tr>
-                    <td>Username: </td>
-                    <td>
-                        <input type="text" name="name" required v-model="user._name">
-                        <p class="error" name="name"  v-if="invalidInput">Invalid name</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Email: </td>
-                    <td>
-                        <input type="email" id="email" required v-model="user._email">
-                        <p id="errorEmail" v-if="invalidInputEmail">{{errorMessageEmail}}</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Address: </td>
-                    <td>
-                        <input type="text" name="address" required v-model="user._address">
-                        <p class="error" v-if="invalidInput">Invalid address</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Phone: </td>
-                    <td>
-                        <input type="tel" name="phone" required v-model="user._phone">
-                        <p class="error" v-if="invalidInput">Invalid phone</p>
-                    </td>
-                </tr>
-            </table>
-            <button @click=verifyInputData>Save changes</button>
-            <p id="changes-saved" v-show="sucessMessageData === true">Changes saved</p>
-            <br>
-            <h1>Change password</h1>
-            <div id="change-password">
-                <h2>Old password</h2>
-                <input type="password" name="old-password" required v-model="old_password">
-                <p class="error" id="password-Invalid">Wrong password</p>
-                <h2>New password</h2>
-                <input type="password" name="new-password" required v-model="new_password">
-                <p class="error" id="password-Identical">Same password</p>
-                <h2>Confirm new password</h2>
-                <input type="password" name="confirm-password" required v-model="confirm_password">
-                <p class="error" id="password-Error">Password doesn't match</p>
-                <button @click=verifyInputPassword>Change password</button>
-                <p id="changes-saved" v-show="sucessMessagePassword === true">Password updated</p>
+            <div id="user-sidebar">
+                <a class="page-select active">User information</a>
+                <router-link class="page-select" to="/history">Purchase history</router-link>
+                <a class="page-select">Logout</a>
+            </div>
+            <div id="user-modification">
+                <h1>User information</h1>
+                <table>
+                    <tr>
+                        <td>Username: </td>
+                        <td>
+                            <input type="text" name="name" required v-model="user._name">
+                            <p class="error" name="name"  v-if="invalidInput">Invalid name</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Email: </td>
+                        <td>
+                            <input type="email" id="email" required v-model="user._email">
+                            <p id="errorEmail" v-if="invalidInputEmail">{{errorMessageEmail}}</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Address: </td>
+                        <td>
+                            <input type="text" name="address" required v-model="user._address">
+                            <p class="error" v-if="invalidInput">Invalid address</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Phone: </td>
+                        <td>
+                            <input type="tel" name="phone" required v-model="user._phone">
+                            <p class="error" v-if="invalidInput">Invalid phone</p>
+                        </td>
+                    </tr>
+                </table>
+                <button @click=verifyInputData>Save changes</button>
+                <p id="changes-saved" v-show="sucessMessageData === true">Changes saved</p>
+                <br>
+                <h1>Change password</h1>
+                <div id="change-password">
+                    <h2>Old password</h2>
+                    <input type="password" name="old-password" required v-model="old_password">
+                    <p class="error" id="password-Invalid">Wrong password</p>
+                    <h2>New password</h2>
+                    <input type="password" name="new-password" required v-model="new_password">
+                    <p class="error" id="password-Identical">Same password</p>
+                    <h2>Confirm new password</h2>
+                    <input type="password" name="confirm-password" required v-model="confirm_password">
+                    <p class="error" id="password-Error">Password doesn't match</p>
+                    <button @click=verifyInputPassword>Change password</button>
+                    <p id="changes-saved" v-show="sucessMessagePassword === true">Password updated</p>
+                </div>
             </div>
         </div>
     </div>
