3439290cdd903363a80540a57527b865de8e560e
Daniel Suzumura <daniel.suzumura@hotmail.com>
2021-07-25 22:40:02 -0300

    remove images

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ app.set('port', port);
 const server = http.createServer(app);
 
 server.listen(port);
-console.log('running on http://localhost:', port);
+console.log('running on http://localhost:'+ port);
